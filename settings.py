e0ada96bb25cb377a74d6d2ff83530bfb359b69b
Hants Williams <hantsawilliams@gmail.com>
2018-12-08 13:56:05 -0500

    Removed my personal DB link

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgresql://hants:46566656@instagram.cjkeu5klelix.us-west-2.rds.amazonaws.com:5432/instadb'
+    'INSERT YOUR DB LINK HERE'
 
 #app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
